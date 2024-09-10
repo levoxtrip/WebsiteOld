9075b9037084967c1b6bc608125d40bd5af3375d
levoxtrip <levoxtrip@gmail.com>
2024-09-10 11:15:18 +0200

    Added animation for intro;

---
+++ js/welcomeAnimation.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const textLines = document.querySelectorAll('.fade-in-text');
+    textLines.forEach((line, index) => {
+        line.style.setProperty('--delay', `${index * 1}s`);
+    });
+});
